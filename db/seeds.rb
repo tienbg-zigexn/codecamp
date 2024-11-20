10e9cbde6a8e58f8cc4c84796cc89ed77a61ebf0
Tien <tienbg@zigexn.vn>
2024-11-20 23:46:04 +0700

    feat: use Faker to seed books

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  Book.create(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    description: Faker::Lorem.paragraphs(number: 2).join('\n')
+  )
+end
