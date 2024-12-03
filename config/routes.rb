a5405eafc89a9d36c3eb903275e808c575c85eb6
Tien <tienbg@zigexn.vn>
2024-12-03 23:55:50 +0700

    perf: limit routes for passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: %i[ new create destroy ]
   resource :registration, only: %i[ new create ]
-  resources :passwords, param: :token
+  resources :passwords, param: :token, only: %i[ new create edit update ]
   root "pages#index"
   resources :books do
     resources :reviews, only: %i[new show create destroy]
