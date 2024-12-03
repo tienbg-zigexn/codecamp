f07a24decf626768dcf7a19735613581c6444396
Tien <tienbg@zigexn.vn>
2024-12-04 04:35:04 +0700

    feat: broadcast follows

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,20 @@
 class Follow < ApplicationRecord
   belongs_to :followed, class_name: "User"
   belongs_to :follower, class_name: "User"
+
+  after_create_commit ->(follow) {
+    broadcast_append_later_to follow.followed, partial: "users/follow", target: helpers.nested_dom_id(follow.followed, "followers"), locals: { user: follow.follower }
+    broadcast_append_later_to follow.follower, partial: "users/follow", target: helpers.nested_dom_id(follow.follower, "following"), locals: { user: follow.followed }
+  }
+
+  after_destroy_commit ->(follow) {
+    broadcast_remove_to follow.followed, target: helpers.nested_dom_id(follow.followed, follow.follower, "follower")
+    broadcast_remove_to follow.follower, target: helpers.nested_dom_id(follow.follower, follow.followed, "following")
+  }
+
+  private
+  
+  def helpers
+    ApplicationController.helpers
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,9 +5,12 @@ class Review < ApplicationRecord
 
   scope :ordered, -> { order(created_at: :desc) }
 
-  include ActionView::RecordIdentifier
-  include ApplicationHelper
+  after_create_commit ->(review) { broadcast_prepend_later_to review.book, partial: 'reviews/review_frame', target: helpers.nested_dom_id(review.book, 'reviews') }
+  after_destroy_commit ->(review) { broadcast_remove_to review.book, target: helpers.nested_dom_id(review.book, review) }
 
-  after_create_commit ->(review) { broadcast_prepend_later_to review.book, partial: 'reviews/review_frame', target: nested_dom_id(review.book, 'reviews') }
-  after_destroy_commit ->(review) { broadcast_remove_to review.book, target: nested_dom_id(review.book, review) }
+  private
+
+  def helpers
+    ApplicationController.helpers
+  end
 end
