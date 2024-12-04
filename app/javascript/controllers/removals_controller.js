0ac12001e3ee34c91fa0f4c469d50a7d06bf219a
Tien <tienbg@zigexn.vn>
2024-12-04 17:47:11 +0700

    feat: flash auto fadeOut

--- app/javascript/controllers/removals_controller.js
+++ app/javascript/controllers/removals_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="removals"
 export default class extends Controller {
   remove() {
-    this.element.parentElement.remove();
+    this.element.remove();
   }
 }
