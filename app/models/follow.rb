976965895aca32b15ed0b7024592242e99e320b9
Tien <tienbg@zigexn.vn>
2024-12-04 08:48:45 +0700

    fix: was not broadcast unfollow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,13 +3,13 @@ class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User"
 
   after_create_commit ->(follow) {
-    broadcast_append_later_to follow.followed, partial: "users/follow", target: helpers.nested_dom_id(follow.followed, "followers"), locals: { user: follow.follower }
-    broadcast_append_later_to follow.follower, partial: "users/follow", target: helpers.nested_dom_id(follow.follower, "following"), locals: { user: follow.followed }
+    broadcast_append_to follow.followed, partial: "users/follow", target: helpers.nested_dom_id(follow.followed, "followers"), locals: { user: follow.follower, relationship: :passive }
+    broadcast_append_to follow.follower, partial: "users/follow", target: helpers.nested_dom_id(follow.follower, "following"), locals: { user: follow.followed, relationship: :active }
   }
 
   after_destroy_commit ->(follow) {
-    broadcast_remove_to follow.followed, target: helpers.nested_dom_id(follow.followed, follow.follower, "follower")
-    broadcast_remove_to follow.follower, target: helpers.nested_dom_id(follow.follower, follow.followed, "following")
+    broadcast_remove_to follow.followed, target: helpers.nested_dom_id(follow.followed, follow.follower, :passive)
+    broadcast_remove_to follow.follower, target: helpers.nested_dom_id(follow.follower, follow.followed, :active)
   }
 
   private
