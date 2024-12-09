f525826cd1ae0db1dcb6b13c85cb24fb3d76a0c5
Tien <tienbg@zigexn.vn>
2024-12-10 04:21:27 +0700

    feat: stop rails generate unit test

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module BookReview
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework false
+    end
   end
 end
