720358c2bd33ec4bf10ccac07dc801bfd6f1a84a
Tien <tienbg@zigexn.vn>
2024-12-10 21:00:24 +0700

    feat: broadcast reviews on profile page as well

--- app/models/review.rb
+++ app/models/review.rb
@@ -14,6 +14,7 @@ class Review < ApplicationRecord
 
   def broadcast_review_after_create
     broadcast_prepend_later_to self.book, partial: 'reviews/review_frame', target: nested_dom_id(self.book, 'reviews')
+    broadcast_append_later_to self.user, partial: 'users/review', target: nested_dom_id(self.user, 'reviews')
   end
 
   def broadcast_review_after_destroy
